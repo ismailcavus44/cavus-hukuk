ee03a8b9018d3e1357ec59d2d9d00b27b9c94745
İsminiz <Email adresiniz>
2025-07-27 17:32:56 +0300

    Fix syntax error in edit blog page

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'ismailcavus.av.tr',
         port: '',
-        pathname: '/*',
       },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
